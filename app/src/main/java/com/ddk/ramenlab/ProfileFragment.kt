8e4b49c4a97b3ddc7cfa484c8ba71e7c4726dd43
Hojun Kim <76956067+hojunkim1@users.noreply.github.com>
2022-12-31 23:49:27 +0900

    update theme and etc

--- app/src/main/java/com/ddk/ramenlab/ProfileFragment.kt
+++ app/src/main/java/com/ddk/ramenlab/ProfileFragment.kt
@@ -1,24 +1,23 @@
 package com.ddk.ramenlab
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.ddk.ramenlab.databinding.FragmentHomeBinding
+import androidx.fragment.app.Fragment
 import com.ddk.ramenlab.databinding.FragmentProfileBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class ProfileFragment : Fragment() {
+
     private lateinit var binding: FragmentProfileBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        super.onCreateView(inflater, container, savedInstanceState)
-        binding = FragmentProfileBinding.inflate(layoutInflater)
+    ): View {
+        binding = FragmentProfileBinding.inflate(inflater)
 
         return binding.root
     }
