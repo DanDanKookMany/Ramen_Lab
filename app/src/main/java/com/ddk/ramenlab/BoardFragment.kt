6ab282ccc1c6438aa50fa44dbfbea796ea029019
JungDohyeon <jdh1109ok@naver.com>
2023-01-04 02:31:37 +0900

    1차
    메뉴 선택 아이콘 수정, 리사이클러뷰 수정해야함

--- app/src/main/java/com/ddk/ramenlab/BoardFragment.kt
+++ app/src/main/java/com/ddk/ramenlab/BoardFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.ddk.ramenlab.adapter.BoardListAdapter
 import com.ddk.ramenlab.databinding.FragmentBoardBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -18,6 +19,33 @@ class BoardFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentBoardBinding.inflate(inflater)
+
+        val recipeBtn = binding.recipeBtn
+        val chatBtn = binding.chatBtn
+        val announceBtn = binding.announceBtn
+
+        // 화면을 처음 키면 레시피가 선택되어있도록 지정
+        recipeBtn.isSelected = true
+
+        recipeBtn.setOnClickListener {
+            recipeBtn.isSelected = true
+            chatBtn.isSelected = false
+            announceBtn.isSelected = false
+        }
+
+        chatBtn.setOnClickListener {
+            chatBtn.isSelected = true
+            recipeBtn.isSelected = false
+            announceBtn.isSelected = false
+        }
+
+        announceBtn.setOnClickListener {
+            announceBtn.isSelected = true
+            recipeBtn.isSelected = false
+            chatBtn.isSelected = false
+        }
+
+        binding.boardRecyclerView.adapter = BoardListAdapter()
 
         return binding.root
     }
