e8c88e5646a7c870d55005ad677ab10724d350b7
JungDohyeon <jdh1109ok@naver.com>
2023-02-14 20:07:57 +0900

    [Add] 게시판 글 읽기 화면

--- app/src/main/java/com/ddk/ramenlab/adapter/RecipeViewPagerAdapter.kt
+++ app/src/main/java/com/ddk/ramenlab/adapter/RecipeViewPagerAdapter.kt
@@ -4,15 +4,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.ddk.ramenlab.R
-import com.ddk.ramenlab.data.recipeImageData
+import com.ddk.ramenlab.data.RecipeImageData
 
 class RecipeViewPagerAdapter: RecyclerView.Adapter<RecipeViewPagerAdapter.RecipeViewPagerViewHolder>(){
-    private val dataset = recipeImageData.newRecipeImg
+    private val dataset = RecipeImageData.newRecipeImg
 
     class RecipeViewPagerViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val image: ImageView = view.findViewById(R.id.recipe_item_img)
+        val content: TextView = view.findViewById(R.id.recipe_content)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecipeViewPagerViewHolder {
@@ -25,6 +27,7 @@ class RecipeViewPagerAdapter: RecyclerView.Adapter<RecipeViewPagerAdapter.Recipe
         val item = dataset[position]
 
         holder.image.setImageResource(item.recipeImageResourceId)
+        holder.content.text = item.content
     }
 
     override fun getItemCount(): Int {
